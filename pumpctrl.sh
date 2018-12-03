30c2b02b784001785eb6eee4c3a54acaa54469a5
colbynh <guitaristcolby@gmail.com>
2018-12-03 10:49:13 -0500

    Fix control pump

--- pumpctrl.sh
+++ pumpctrl.sh
@@ -33,7 +33,7 @@ function lightOff() {
 
 # controlPump takes an parameter of minutes to run the pump
 function controlPump() {
-    $PUMP_RUN_TIME=$1
+    PUMP_RUN_TIME=$1
     
     startpump
 
@@ -45,15 +45,18 @@ function controlPump() {
 DEFAULT_PUMP_RUNTIME=4m
 
 main() {
+    PUMP_RUN_TIME=$1
     if [ -n $PUMP_RUN_TIME ];
     then
+        echo "running custom pump time"
         # Run the pump for n amount for minutes
         controlPump $PUMP_RUN_TIME
     else
         # default the pump to run for 4 minutes
+        echo "running pump for 4 minutes"
         controlPump $DEFAULT_PUMP_RUNTIME
     fi
 }
 
-
+main "5m"
 
