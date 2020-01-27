e509f9c6fb5dfd55ae37413a6f07e64d644758fb
colbynh <guitaristcolby@gmail.com>
2020-01-27 18:46:58 -0500

    Make pumpctrl more custom

--- pumpctrl.sh
+++ pumpctrl.sh
@@ -38,34 +38,41 @@ function stoppump() {
 function controlPump() {
     while [ true ]
     do
-        PUMP_RUN_TIME=$1
-
+        local PUMP_RUN_TIME=$1
+        local PUMP_SLEEPTIME=$2
+            
         startpump
 
         sleep $PUMP_RUN_TIME
 
         stoppump
         
-        sleep 30m
+        sleep $PUMP_SLEEPTIME
     done      
 
 }
 
 DEFAULT_PUMP_RUNTIME=4m
+DEFAULT_PUMP_SLEEPTIME=30m
 
 main() {
     PUMP_RUN_TIME=$1
+    PUMP_SLEEPTIME=$2
     if [ -n $PUMP_RUN_TIME ];
     then
         echo "running custom pump time"
-        # Run the pump for n amount for minutes
-        controlPump $PUMP_RUN_TIME
+        # Run the pump for n amount for minutes and sleep for n minutes
+        controlPump $PUMP_RUN_TIME $PUMP_SLEEPTIME
     else
         # default the pump to run for 4 minutes
         echo "running pump for 4 minutes"
-        controlPump $DEFAULT_PUMP_RUNTIME
+        controlPump $DEFAULT_PUMP_RUNTIME $DEFAULT_PUMP_RUNTIME
     fi
 }
 
-main "5m"
+# call the script with the main function and two args
+# runtime and sleep time... Default is 4mins and 30 mins
+# ex: . ./pumpctrl.sh && main 5m 20m
+
+main 
 
