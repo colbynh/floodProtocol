085cb730782a9ebb26576e4ff01ce94a2544c5ec
Colby Chenard <guitaristcolby@gmail.com>
2021-04-06 09:23:31 -0400

    Update deploy scripts

--- update-service.sh
+++ update-service.sh
@@ -5,14 +5,19 @@ set -e
 # set username and password
 UNAME="guitaristcolby"
 UPASS=$DOCKER_PASS
+TOKEN=""
+REPO_LIST=""
 
-# get token to be able to talk to Docker Hub
-TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${UNAME}'", "password": "'${UPASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+getRepoList() {
+    # get token to be able to talk to Docker Hub
+    TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${UNAME}'", "password": "'${UPASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+    # get list of repos for that user account
+    REPO_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${UNAME}/?page_size=10000 | jq -r '.results|.[]|.name')
 
-# get list of repos for that user account
-REPO_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${UNAME}/?page_size=10000 | jq -r '.results|.[]|.name')
+}
 
-getImageList() {
+getLatestImageNum() {
+    getRepoList
     # build a list of all images & tags
     for i in ${REPO_LIST}
     do
@@ -43,20 +48,19 @@ getImageList() {
     echo $maxtag
 }
 
+checkForUpdate() {
+    while [ true ]
+    do 
+        maxtag=$(getLatestImageNum)
+        svctag=$(docker service ls | awk '{print $5}' | grep flood-prod | cut -d ":" -f 2 | tail -n 1)
+        echo $maxtag
+        echo $svctag
+        if [[ "$maxtag" -gt "$svctag" ]]
+        then    
+            docker service update --force --image guitaristcolby/flood-control:$svctag flood-prod
+        fi
+        sleep 5
+    done
+}
 
-while [ true ]
-do 
-    maxtag=$(getImageList)
-    svctag=$(docker service ls | awk '{print $5}' | grep flood | cut -d ":" -f 2 | tail -n 1)
-    echo $maxtag
-    echo $svctag
-    if [[ "$maxtag" -gt "$svctag" ]]
-    then    
-        docker service update --force --image guitaristcolby/flood-control:$maxtag flood_web-app
-    fi
-    sleep 5
-done
-
-
-
-
+checkForUpdate
